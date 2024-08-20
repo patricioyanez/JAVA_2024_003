39127b02aebcad46002350512778d7d4a9e3d476
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-20 17:04:41 -0400

    update

--- Proyecto2_003/src/Principal.java
+++ Proyecto2_003/src/Principal.java
@@ -10,6 +10,13 @@ public class Principal {
         pizza1.setMasa("Delgada");
         pizza1.setTamano("Familiar");
         pizza1.setPrecio(10000);
+        
+        System.out.println("== Datos de la Pizza ==");
+        System.out.println("Nombre  : " + pizza1.getNombre());
+        System.out.println("Masa    : " + pizza1.getMasa());
+        System.out.println("Tamaño  : " + pizza1.getTamano());
+        System.out.println("Precio  : " + pizza1.getPrecio());
+        
 
     }
 }
