a15b5a187e0f72b03120eaed8d5f2de9b0af9e08
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-08-20 17:18:43 -0400

    update

--- Proyecto2_003/src/Principal.java
+++ Proyecto2_003/src/Principal.java
@@ -17,6 +17,18 @@ public class Principal {
         System.out.println("Tamaño  : " + pizza1.getTamano());
         System.out.println("Precio  : " + pizza1.getPrecio());
         
-
+        // Ejercicio 2: CRear otra pizza y mostrar los datos
+        Pizza pizza2 = new Pizza();
+        
+        pizza2.setNombre("Napolitana");
+        pizza2.setMasa("Borde Queso");
+        pizza2.setTamano("XL");
+        pizza2.setPrecio(12000);
+        
+        System.out.println("== Datos de la Pizza ==");
+        System.out.println("Nombre  : " + pizza2.getNombre());
+        System.out.println("Masa    : " + pizza2.getMasa());
+        System.out.println("Tamaño  : " + pizza2.getTamano());
+        System.out.println("Precio  : " + pizza2.getPrecio());
     }
 }
