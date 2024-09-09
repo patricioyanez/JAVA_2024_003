f6d1813532227ccc9a6bb432fd4c6923c65c6584
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-09 16:03:37 -0300

    update

--- Proyecto3_003_Colaboracion/src/Principal.java
+++ Proyecto3_003_Colaboracion/src/Principal.java
@@ -1,9 +1,15 @@
-
 public class Principal {
 
     public static void main(String[] args) {
         Marca marca1 = new Marca(100, "CocaCola", "Juan", "65498765");
+        Marca marca2 = new Marca();
+        marca2.setCodigo(200);
+        marca2.setNombre("pepsi");
+        marca2.setContacto("Ana");
+        marca2.setTelefono("654659873");
+        
         Producto producto1  = new Producto();
+        Producto producto2 = new Producto(300, "3lt Desechable", marca2, 3000, 30);
 //        System.out.println(producto1.imprimirDatos());
         
         producto1.setCodigo(1000);
@@ -13,8 +19,12 @@ public class Principal {
         producto1.setMarca(marca1);
         System.out.println(producto1.imprimirDatos());
         
+        System.out.println(producto2.imprimirDatos());
+        System.out.println("\n\n");
         System.out.println(producto1.getMarca().getNombre());
         System.out.println(producto1.getMarca().getContacto());
+        
+        // crear un 2do producto de marca pepsi, imprimir datos
     }
     
 }
