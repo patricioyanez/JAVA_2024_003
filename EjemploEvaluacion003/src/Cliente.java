7cd1b75ef189012d83fb8324a2483d8eba8fb76a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-10 08:47:37 -0300

    update

---
+++ EjemploEvaluacion003/src/Cliente.java
@@ -0,0 +1,83 @@
+
+public class Cliente {
+    private String rut;
+    private String nombre;
+    private int codigoArea;
+    private int telefono;
+    private String correo;
+    private boolean esNuevo;
+
+    public Cliente() {
+        this.rut = "";
+        this.nombre = "";
+        this.codigoArea = 0;
+        this.telefono = 0;
+        this.correo = "";
+        this.esNuevo = false;
+    }
+    
+    public Cliente(String rut, String nombre, int codigoArea, int telefono, String correo, boolean esNuevo) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.codigoArea = codigoArea;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.esNuevo = esNuevo;
+    }
+
+    public boolean isEsNuevo() {
+        return esNuevo;
+    }
+
+    public void setEsNuevo(boolean esNuevo) {
+        this.esNuevo = esNuevo;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigoArea() {
+        return codigoArea;
+    }
+
+    public void setCodigoArea(int codigoArea) {
+        this.codigoArea = codigoArea;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    
+    public String imprimirDatos() {
+        return "Cliente{" + "rut=" + rut + ", nombre=" + nombre + ", codigoArea=" + codigoArea + ", telefono=" + telefono + ", correo=" + correo + ", esNuevo=" + esNuevo + '}';
+    }
+    
+    
+    
+}
---
+++ EjemploEvaluacion003/src/Vehiculo.java
@@ -0,0 +1,76 @@
+public class Vehiculo {
+    private String patente;
+    private String marca;
+    private String modelo;
+    private int kilometraje;
+    private int costoServicio;
+    private Cliente cliente;
+
+    public Vehiculo() {
+        this.patente = "";
+        this.marca = "";
+        this.modelo = "";
+        this.kilometraje = 0;
+        this.costoServicio = 0;
+        this.cliente = null; // new Cliente();
+    }
+    
+    public Vehiculo(String patente, String marca, String modelo, int kilometraje, int costoServicio, Cliente cliente) {
+        this.patente = patente;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.kilometraje = kilometraje;
+        this.costoServicio = costoServicio;
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(int kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public int getCostoServicio() {
+        return costoServicio;
+    }
+
+    public void setCostoServicio(int costoServicio) {
+        this.costoServicio = costoServicio;
+    }
+    // metodos personalizados...
+    
+}
