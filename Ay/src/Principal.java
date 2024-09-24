7441a796c42920869a35ab31393e01e1f246eb3f
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-24 16:32:26 -0300

    update

--- Ay/src/Principal.java
+++ Ay/src/Principal.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Crear un menu que permita ingresar nombres, 
 // apellidos y sus notas y 
 // otra para listarlos.
@@ -5,7 +8,20 @@
 public class Principal {
 
     public static void main(String[] args) {
+        ArrayList<String> nombres = new ArrayList<String>();
+        ArrayList<Integer> notas = new ArrayList<Integer>();
+        Scanner leerTeclado = new Scanner(System.in);
+        int opcion = 0;
         
+        while(true)
+        {
+            System.out.println("|======= Menú ========|");
+            System.out.println("1.-Ingresar datos");
+            System.out.println("2.-Listar");
+            System.out.println("3.-Salir");
+            System.out.print("Ingrese opción: ");
+            opcion = leerTeclado.nextInt();
+        }
         
     }
     
