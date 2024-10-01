d329e497c11ebd8116a45c6a7a1e5b9364a9612a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-01 16:45:29 -0300

    update

---
+++ Herencia003/src/Cliente.java
@@ -0,0 +1,42 @@
+
+public class Cliente extends Persona {
+    private Boolean esVip;
+    private int lineaCredito;
+
+    public Cliente() {
+        this.esVip = false;
+        this.lineaCredito = 0;
+    }
+
+    public Cliente(Boolean esVip, int lineaCredito, String rut, String nombre, int anioNacimiento, String direccion) {
+        super(rut, nombre, anioNacimiento, direccion);
+        this.esVip = esVip;
+        this.lineaCredito = lineaCredito;
+    }
+
+    public int getLineaCredito() {
+        return lineaCredito;
+    }
+
+    public void setLineaCredito(int lineaCredito) {
+        this.lineaCredito = lineaCredito;
+    }
+
+    public Boolean getEsVip() {
+        return esVip;
+    }
+
+    public void setEsVip(Boolean esVip) {
+        this.esVip = esVip;
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() +
+                "\nEs Vip       = " + (esVip?"Si":"No") + 
+                "\nLinea Crédito= " + lineaCredito;
+    }
+    
+    
+    
+}
--- Herencia003/src/Pricipal.java
+++ Herencia003/src/Pricipal.java
@@ -4,7 +4,13 @@
 public class Pricipal {
 
     public static void main(String[] args) {
+        Cliente cliente = new Cliente(true, 150000, "2-7", "Ana", 2000, "Vespucio 1501");
+        Empleado empleado = new Empleado(550000, "Vendedor", "1-9", "Luis", 1990, "Cerrillos 50");
 
+        System.out.println("===== Listado de cliente =====");
+        System.out.println(cliente);
+        System.out.println("\n===== Listado de Empleados =====");
+        System.out.println(empleado);
     }
     
 }
