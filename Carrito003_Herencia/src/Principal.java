a47651c6831dffb1dfef03ae8c112675697ae3ea
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-07 16:27:10 -0300

    update

--- Carrito003_Herencia/src/Principal.java
+++ Carrito003_Herencia/src/Principal.java
@@ -1,20 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author patri
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        Electronico e1 = new Electronico("Sorny", 12, "3", "Tv", 25000.0);
+        Ropa r1 = new Ropa("s", "Blanco", "5", "Polera", 5000.0);
+        
+        System.out.println(e1);
+        e1.extenderGarantia(10);
+        System.out.println(e1);
+        
+        
+        System.out.println(r1);
+        r1.ajustarTalla("l");
+        System.out.println(r1);
+        
     }
     
 }
