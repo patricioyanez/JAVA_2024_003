9b4d1581e6bf19437b97adc3aec9716482c421d9
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-22 16:20:48 -0300

    update

--- EjercicioTipoPrueba2_003/src/Auto.java
+++ EjercicioTipoPrueba2_003/src/Auto.java
@@ -1,4 +1,67 @@
 
-public class Auto{
+public class Auto extends Vehiculo {
+    private int cantidadPuerta;
+    private int capacidadMaletero;
+
+    public Auto() {
+        this.cantidadPuerta = 0;
+        this.capacidadMaletero = 0;
+    }
+
+    public Auto(int cantidadPuerta, int capacidadMaletero, String patente, String marca, String modelo, String dueno, int precioServicio, int iva, int totalCobrar) {
+        super(patente, marca, modelo, dueno, precioServicio, iva, totalCobrar);
+        this.cantidadPuerta = cantidadPuerta;
+        this.capacidadMaletero = capacidadMaletero;
+    }
+
+    public int getCapacidadMaletero() {
+        return capacidadMaletero;
+    }
+
+    public void setCapacidadMaletero(int capacidadMaletero) {
+        this.capacidadMaletero = capacidadMaletero;
+    }
+
+    public int getCantidadPuerta() {
+        return cantidadPuerta;
+    }
+
+    public void setCantidadPuerta(int cantidadPuerta) {
+        this.cantidadPuerta = cantidadPuerta;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+                "\nCantidad de Puertas  = " + cantidadPuerta + 
+                "\nCapacidad de Maletero= " + capacidadMaletero;
+    }
+
+    @Override
+    public int totalPorPagar() {
+        int resultado = this.getPrecioServicio() + getIva();
+        this.setTotalCobrar(resultado);
+        return resultado;
+    }
+
+    @Override
+    public int totalConIVA() {
+        int resultado = (int)(this.getPrecioServicio() * IVA);
+        this.setIva(resultado);
+        return resultado;
+    }
+
+    @Override
+    public int obtenerDescuento() {
+        int resultado = (int)(this.getPrecioServicio() * DESCUENTO_AUTO);
+        this.setPrecioServicio(this.getPrecioServicio() - resultado);
+        return resultado;
+    }
+    
+    public int obtenerDescuento(double descuento) {
+        int resultado = (int)(this.getPrecioServicio() * descuento);
+        this.setPrecioServicio(this.getPrecioServicio() - resultado);
+        return resultado;
+    }
     
 }
--- EjercicioTipoPrueba2_003/src/Taller.java
+++ EjercicioTipoPrueba2_003/src/Taller.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+
 /*
 Clase gestora
-
 contine el ArrayList
-
 Generar metodos CRUD
 */
 public class Taller {
-    
+    ArrayList<Vehiculo> listado = new ArrayList<Vehiculo>();
+    public boolean agregar(Vehiculo vehiculo)
+    {
+        // ejercicio 31: validar que patente no existe para 
+        // agregar
+        return listado.add(vehiculo);
+    }
+    // Ejercicio 30: crear metodo para saber si la patente ya existe 
+    public void listar()
+    {
+        System.out.println("==== Listado de Vehiculos =====");
+        for (Vehiculo vehiculo : listado) {
+            System.out.println(vehiculo);
+        }
+    }
 }
